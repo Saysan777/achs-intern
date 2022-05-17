8a729d7e9fe6661dd2f2da03543164c7ebf0165b
Alok <alokaryal32@gmail.com>
2022-05-17 21:12:16 +0545

    first

---
+++ server/main.js
@@ -0,0 +1,8 @@
+// import "../imports/api/fixtures";
+import { links } from "../imports/api/collections/Links.js";
+import { Meteor } from "meteor/meteor";
+
+// Meteor.startup(() => {
+//   process.env.MONGODB_URI
+// })
+// 
